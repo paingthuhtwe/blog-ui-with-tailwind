6442a6c52ae0473dea9d113266f37d335e0901db
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-06-25 21:06:04 +0630

    add scroll reveal in chart page

--- src/components/ChartPage.vue
+++ src/components/ChartPage.vue
@@ -2,23 +2,23 @@
   <div class="bg-indigo-500">
     <div class="w-10/12 mx-auto">
       <div class="grid grid-cols-1 md:grid-cols-4">
-        <div class="text-center my-14 md:border-r-2">
+        <div class="text-center my-14 md:border-r-2 scroll-bottom">
           <h1 class="text-white text-4xl font-bold">8K+</h1>
           <h1 class="text-slate-200 text-md font-semibold">Success Stories</h1>
         </div>
-        <div class="text-center my-14 md:border-r-2">
+        <div class="text-center my-14 md:border-r-2 scroll-bottom-1">
           <h1 class="text-white text-4xl font-bold">200+</h1>
           <h1 class="text-slate-200 text-md font-semibold">
             Expert Instructor
           </h1>
         </div>
-        <div class="text-center my-14 md:border-r-2">
+        <div class="text-center my-14 md:border-r-2 scroll-bottom-2">
           <h1 class="text-white text-4xl font-bold">108K+</h1>
           <h1 class="text-slate-200 text-md font-semibold">
             Worldwide Students
           </h1>
         </div>
-        <div class="text-center my-14">
+        <div class="text-center my-14 scroll-bottom-3">
           <h1 class="text-white text-4xl font-bold">310+</h1>
           <h1 class="text-slate-200 text-md font-semibold">Trendy Subjects</h1>
         </div>
