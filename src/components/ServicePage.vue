a2d12677c24062771be03e860d610bab0758dfa9
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-06-25 21:10:04 +0630

    add scroll reveal in service page

--- src/components/ServicePage.vue
+++ src/components/ServicePage.vue
@@ -3,22 +3,22 @@
     <div class="w-10/12 mx-auto space-y-8">
       <div class="lg:flex justify-between space-y-8 lg:scroll-py-0">
         <div class="lg:w-2/5 p-5">
-          <h1 class="text-4xl font-bold text-slate-700">
+          <h1 class="text-4xl font-bold text-slate-700 scroll-left">
             Why we are best from others?
           </h1>
-          <p class="text-slate-500 text-md font-medium mt-3">
+          <p class="text-slate-500 text-md font-medium mt-3 scroll-right">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
             commodi alias deleniti eveniet non reprehenderit ad voluptatum nemo.
           </p>
           <button
-            class="text-slate-800 bg-yellow-400 w-36 h-10 text-md font-semibold rounded-full hover:bg-yellow-300 hover:text-slate-700 mt-6"
+            class="text-slate-800 bg-yellow-400 w-36 h-10 text-md font-semibold rounded-full hover:bg-yellow-300 hover:text-slate-700 mt-6 scroll-bottom"
           >
             Start Now
           </button>
         </div>
         <div class="lg:w-3/5 lg:flex justify-end gap-8 space-y-8 lg:space-y-0">
           <div
-            class="bg-white shadow-md border shadow-indigo-100 hover:shadow-indigo-100 border-slate-100 p-7 rounded-lg max-w-[360px] min-h-[320px] hover:shadow-lg"
+            class="bg-white shadow-md border shadow-indigo-100 hover:shadow-indigo-100 border-slate-100 p-7 rounded-lg max-w-[360px] min-h-[320px] hover:shadow-lg scroll-bottom"
           >
             <fa
               class="w-10 h-10 bg-purple-500 p-2 text-white rounded-md mb-2 shadow-md shadow-purple-200"
@@ -35,7 +35,7 @@
             </p>
           </div>
           <div
-            class="bg-white shadow-md border shadow-indigo-100 hover:shadow-indigo-100 border-slate-100 p-7 rounded-lg max-w-[360px] min-h-[320px] hover:shadow-lg"
+            class="bg-white shadow-md border shadow-indigo-100 hover:shadow-indigo-100 border-slate-100 p-7 rounded-lg max-w-[360px] min-h-[320px] hover:shadow-lg scroll-bottom-1"
           >
             <fa
               class="w-10 h-10 bg-lime-500 p-2 text-white rounded-md mb-2 shadow-md shadow-lime-200"
@@ -55,7 +55,7 @@
       </div>
       <div class="lg:flex justify-end gap-8 space-y-8 lg:space-y-0">
         <div
-          class="bg-white shadow-md border shadow-indigo-100 hover:shadow-indigo-100 border-slate-100 p-7 rounded-lg max-w-[360px] min-h-[320px] hover:shadow-lg"
+          class="bg-white shadow-md border shadow-indigo-100 hover:shadow-indigo-100 border-slate-100 p-7 rounded-lg max-w-[360px] min-h-[320px] hover:shadow-lg scroll-bottom-2"
         >
           <fa
             class="w-10 h-10 bg-sky-500 p-2 text-white rounded-md mb-2 shadow-md shadow-sky-200"
@@ -72,7 +72,7 @@
           </p>
         </div>
         <div
-          class="bg-white shadow-md border shadow-indigo-100 hover:shadow-indigo-100 border-slate-100 p-7 rounded-lg max-w-[360px] min-h-[320px] hover:shadow-lg"
+          class="bg-white shadow-md border shadow-indigo-100 hover:shadow-indigo-100 border-slate-100 p-7 rounded-lg max-w-[360px] min-h-[320px] hover:shadow-lg scroll-bottom-3"
         >
           <fa
             class="w-10 h-10 bg-pink-500 p-2 text-white rounded-md mb-2 shadow-md shadow-pink-200"
@@ -87,7 +87,7 @@
           </p>
         </div>
         <div
-          class="bg-white shadow-md border shadow-indigo-100 hover:shadow-indigo-100 border-slate-100 p-7 rounded-lg max-w-[360px] min-h-[320px] hover:shadow-lg"
+          class="bg-white shadow-md border shadow-indigo-100 hover:shadow-indigo-100 border-slate-100 p-7 rounded-lg max-w-[360px] min-h-[320px] hover:shadow-lg scroll-bottom-4"
         >
           <fa
             class="w-10 h-10 bg-rose-500 p-2 text-white rounded-md mb-2 shadow-md shadow-rose-200"
