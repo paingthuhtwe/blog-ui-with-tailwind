83613b559701786d5fd6f8cb61601a3c3ef502f9
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-06-25 21:13:49 +0630

    add scroll reveal in category page

--- src/components/CategoryPage.vue
+++ src/components/CategoryPage.vue
@@ -2,12 +2,12 @@
   <div class="">
     <div class="w-10/12 mx-auto my-12">
       <h1
-        class="text-3xl md:text-5xl text-center text-slate-700 font-semibold mb-6"
+        class="text-3xl md:text-5xl text-center text-slate-700 font-semibold mb-6 scroll-left"
       >
         Browse Course by categories
       </h1>
       <p
-        class="text-md text-slate-400 font-medium md:w-3/5 mx-auto text-center mb-6"
+        class="text-md text-slate-400 font-medium md:w-3/5 mx-auto text-center mb-6 scroll-right"
       >
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae repellat
         deleniti molestiae? Neque, rem deleniti illo soluta, omnis assumenda
@@ -37,7 +37,7 @@
         >
           <swiper-slide>
             <div
-              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50"
+              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50 scroll-bottom"
             >
               <fa
                 class="text-3xl text-indigo-500"
@@ -48,7 +48,7 @@
           </swiper-slide>
           <swiper-slide>
             <div
-              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50"
+              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50 scroll-bottom-1"
             >
               <fa
                 class="text-3xl text-indigo-500"
@@ -61,7 +61,7 @@
           </swiper-slide>
           <swiper-slide>
             <div
-              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50"
+              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50 scroll-bottom-2"
             >
               <fa
                 class="text-3xl text-indigo-500"
@@ -74,7 +74,7 @@
           </swiper-slide>
           <swiper-slide>
             <div
-              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50"
+              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50 scroll-bottom-3"
             >
               <fa
                 class="text-3xl text-indigo-500"
@@ -87,7 +87,7 @@
           </swiper-slide>
           <swiper-slide>
             <div
-              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50"
+              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50 scroll-bottom-4"
             >
               <fa class="text-3xl text-indigo-500" :icon="['fas', 'book']" />
               <h1 class="text-lg font-semibold text-slate-500 mt-3">English</h1>
@@ -95,7 +95,7 @@
           </swiper-slide>
           <swiper-slide>
             <div
-              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50"
+              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50 scroll-bottom-4"
             >
               <fa class="text-3xl text-indigo-500" :icon="['fas', 'mobile']" />
               <h1 class="text-lg font-semibold text-slate-500 mt-3">Mobile</h1>
@@ -103,7 +103,7 @@
           </swiper-slide>
           <swiper-slide>
             <div
-              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50"
+              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50 scroll-bottom-4"
             >
               <fa class="text-3xl text-indigo-500" :icon="['fas', 'globe']" />
               <h1 class="text-lg font-semibold text-slate-500 mt-3">Web Dev</h1>
@@ -111,7 +111,7 @@
           </swiper-slide>
           <swiper-slide>
             <div
-              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50"
+              class="bg-white shadow-lg rounded-lg w-[150px] h-[150px] flex flex-col text-center justify-center items-center border border-indigo-50 scroll-bottom-4"
             >
               <fa class="text-3xl text-indigo-500" :icon="['fas', 'server']" />
               <h1 class="text-lg font-semibold text-slate-500 mt-3">
